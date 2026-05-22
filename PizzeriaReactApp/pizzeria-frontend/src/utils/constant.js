a6c95daf9b463c681f361560ba1e7d99479328b7
Harsh <harshgupta0038@gmail.com>
2026-05-22 14:41:41 +0530

    update API_BASE_URL to point to the production server

--- PizzeriaReactApp/pizzeria-frontend/src/utils/constant.js
+++ PizzeriaReactApp/pizzeria-frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api/v1";
+export const API_BASE_URL = "https://pizzeria-d2ss.vercel.app";
 
 export const PIZZA_API = `${API_BASE_URL}/pizzas`;
 export const INGREDIENT_API = `${API_BASE_URL}/ingredients`;
