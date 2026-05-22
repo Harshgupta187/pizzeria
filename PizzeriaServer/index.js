3b0d8673735975507554aaa114c84141dd8818ee
Harsh <harshgupta0038@gmail.com>
2026-05-22 14:46:40 +0530

    fix: revert API_BASE_URL to local development and update CORS origin

--- PizzeriaServer/index.js
+++ PizzeriaServer/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption={
-    origin:'https://pizzeria-ecru-five.vercel.app',
+    origin:'http://localhost:5173',
     credentials:true
 };
 app.use(cors(corsOption));
