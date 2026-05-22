043f535c4e4a0e772372bb7664a605aabc965cd1
Harsh <harshgupta0038@gmail.com>
2026-05-22 14:44:00 +0530

    fix: update CORS origin URL by removing trailing slash

--- PizzeriaServer/index.js
+++ PizzeriaServer/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption={
-    origin:'https://pizzeria-ecru-five.vercel.app/',
+    origin:'https://pizzeria-ecru-five.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption));
